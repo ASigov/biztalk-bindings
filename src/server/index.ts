b0dc55746df7f92aa05744cdcbdd1404387cb70d
Alexey Sigov <opminded@gmail.com>
2019-07-20 22:24:19 +0300

    Replace console.log with debug

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,14 @@
 import express from 'express';
+import debug from 'debug';
+
+process.env.DEBUG = '*';
 
 const app = express();
+const log = debug('biztalk-bindings:server');
 
 app.use(express.static('public'));
 app.use(express.static('dist/client'));
 app.use(express.static('node_modules/react/umd'));
 app.use(express.static('node_modules/react-dom/umd'));
 
-app.listen(3000, (): void => console.log('Listening on port 3000...'));
+app.listen(3000, (): void => log('Listening on port 3000...'));
