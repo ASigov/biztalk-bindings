79ef3d1b831ada84783f6e03f4a8f6bb4884b132
Alexey Sigov <opminded@gmail.com>
2019-07-23 14:28:47 +0300

    Create initial app markup

--- src/client/components/App.tsx
+++ src/client/components/App.tsx
@@ -1,8 +1,115 @@
 import React from 'react';
 
-const App: React.FC = (): JSX.Element => {
-  const result = <div>Hello React</div>;
-  return result;
-};
+const App: React.FC = (): JSX.Element => (
+  <div className="container">
+    <div className="row">
+      <div className="col mt-3">
+        <div className="custom-file">
+          <label className="custom-file-label" htmlFor="customFile">
+            Choose bindings file
+            <input className="custom-file-input" type="file" id="customFile" />
+          </label>
+        </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col mt-3">
+        <h4>Application</h4>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="false"
+          >
+            OCCM.EDI.Router
+          </button>
+          <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <button className="dropdown-item" type="button">
+              OCCM.EDI.Billing.In
+            </button>
+            <button className="dropdown-item" type="button">
+              OCCM.EDI.Billing.Out
+            </button>
+            <button className="dropdown-item" type="button">
+              OCCM.EDI.Router
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col mt-3">
+        <h4>Send Ports</h4>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <div className="list-group">
+          <button
+            className="list-group-item list-group-item-action active"
+            type="button"
+          >
+            EDI.Router.SP.ROUTER.WCEDI.BILL.TRP.WC.R.IN
+          </button>
+          <button
+            className="list-group-item list-group-item-action"
+            type="button"
+          >
+            EDI.Router.SP.ROUTER.WCEDI.BILL.TRP.WC.R.OUT
+          </button>
+          <button
+            className="list-group-item list-group-item-action"
+            type="button"
+          >
+            EDI.Router.SP.ROUTER.WCEDI.TIN.TRP.WC.R.OUT
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col mt-3">
+        <h4>Receive Locations</h4>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col">
+        <div className="list-group">
+          <button
+            className="list-group-item list-group-item-action active"
+            type="button"
+          >
+            EDI.Router.RL.ROUTER.WCEDI.BILL.TRP.WC.R.IN
+          </button>
+          <button
+            className="list-group-item list-group-item-action"
+            type="button"
+          >
+            EDI.Router.RL.ROUTER.WCEDI.BILL.TRP.WC.R.OUT
+          </button>
+          <button
+            className="list-group-item list-group-item-action"
+            type="button"
+          >
+            EDI.Router.RL.ROUTER.WCEDI.TIN.TRP.WC.R.OUT
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="row">
+      <div className="col mt-3">
+        <button className="btn btn-primary btn-lg btn-block" type="button">
+          Generate OCCM.EDI.Router bindings
+        </button>
+      </div>
+    </div>
+  </div>
+);
 
 export default App;
