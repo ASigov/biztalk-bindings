b466688dc003665b5828377ab0a9dfa783369b5c
Alexey Sigov <opminded@gmail.com>
2019-07-23 14:27:34 +0300

    Add bootstrap
    Add bootstrap and jquery packages.
    Update index.html to include bootstrap and dependencies.
    Update server to serve bootstrap and dependencies.

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,5 +8,8 @@ app.use(express.static('public'));
 app.use(express.static('dist/client'));
 app.use(express.static('node_modules/react/umd'));
 app.use(express.static('node_modules/react-dom/umd'));
+app.use(express.static('node_modules/bootstrap/dist/css'));
+app.use(express.static('node_modules/bootstrap/dist/js'));
+app.use(express.static('node_modules/jquery/dist'));
 
 app.listen(3000, (): void => log('Listening on port 3000...'));
