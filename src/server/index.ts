1b5ea930130d3c6a8318950a8967176c0ba33338
Alexey Sigov <opminded@gmail.com>
2019-07-26 16:04:14 +0300

    Add popper.js

--- src/server/index.ts
+++ src/server/index.ts
@@ -10,12 +10,13 @@ const formDataHandler = multer();
 const log = debug('biztalk-bindings:server');
 
 app.use(express.static('dist/client'));
-app.use(express.static('node_modules/react/umd'));
-app.use(express.static('node_modules/react-dom/umd'));
+app.use(express.static('node_modules/jquery/dist'));
+app.use(express.static('node_modules/popper.js/dist/umd'));
 app.use(express.static('node_modules/bootstrap/dist/css'));
 app.use(express.static('node_modules/bootstrap/dist/js'));
-app.use(express.static('node_modules/jquery/dist'));
 app.use(express.static('node_modules/bs-custom-file-input/dist'));
+app.use(express.static('node_modules/react/umd'));
+app.use(express.static('node_modules/react-dom/umd'));
 
 app.post(
   '/upload',
