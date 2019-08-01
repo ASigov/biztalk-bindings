568a92b1a30f117e35137638b6b094e3a9f1dcb8
Alexey Sigov <opminded@gmail.com>
2019-08-01 11:15:06 +0300

    Extract mapParserState to separate file

---
+++ src/server/parser/mapParserState.ts
@@ -0,0 +1,65 @@
+import ParserState from './ParserState';
+import {
+  SendPort,
+  ReceiveLocation,
+  Application,
+  Bindings,
+} from '../../shared/model';
+import ReceiveLocationState from './ReceiveLocationState';
+
+const getApplicationNames = (state: ParserState): string[] => {
+  return state.receivePorts
+    .map((rp): string => rp.applicationName)
+    .concat(state.sendPorts.map((sp): string => sp.applicationName))
+    .filter((val, index, self): boolean => self.indexOf(val) === index);
+};
+
+const mapSendPorts = (
+  state: ParserState,
+  applicationName: string,
+): SendPort[] => {
+  return state.sendPorts
+    .filter((sp): boolean => sp.applicationName === applicationName)
+    .map(
+      (sp): SendPort => ({
+        name: sp.name,
+        address: sp.address,
+      }),
+    )
+    .sort((left, right): number => left.name.localeCompare(right.name));
+};
+
+const mapReceiveLocations = (
+  state: ParserState,
+  applicationName: string,
+): ReceiveLocation[] => {
+  return state.receivePorts
+    .filter((rp): boolean => rp.applicationName === applicationName)
+    .map((rp): ReceiveLocationState[] => rp.receiveLocations)
+    .reduce((prev, curr): ReceiveLocationState[] => prev.concat(curr), [])
+    .map(
+      (rl): ReceiveLocation => ({
+        name: rl.name,
+        address: rl.address,
+      }),
+    )
+    .sort((left, right): number => left.name.localeCompare(right.name));
+};
+
+const mapApplications = (state: ParserState): Application[] => {
+  return getApplicationNames(state)
+    .map(
+      (name): Application => ({
+        name,
+        sendPorts: mapSendPorts(state, name),
+        receiveLocations: mapReceiveLocations(state, name),
+      }),
+    )
+    .sort((left, right): number => left.name.localeCompare(right.name));
+};
+
+const mapParserState = (state: ParserState): Bindings => ({
+  applications: mapApplications(state),
+});
+
+export default mapParserState;
--- src/server/parser/parseBindings.ts
+++ src/server/parser/parseBindings.ts
@@ -1,12 +1,7 @@
 import { Parser, XmlAttributes } from 'node-expat';
+import { Bindings } from '../../shared/model';
 import ParserState from './ParserState';
-import ReceiveLocationState from './ReceiveLocationState';
-import {
-  Bindings,
-  Application,
-  SendPort,
-  ReceiveLocation,
-} from '../../shared/model';
+import mapParserState from './mapParserState';
 
 const handleStartElement = (
   state: ParserState,
@@ -37,63 +32,6 @@ const handleText = (state: ParserState, text: string): void => {
   } else if (state.pathEndsWith(['ReceiveLocation', 'Address'])) {
     state.setRLAddress(text);
   }
-};
-
-const getApplicationNames = (state: ParserState): string[] => {
-  return state.receivePorts
-    .map((rp): string => rp.applicationName)
-    .concat(state.sendPorts.map((sp): string => sp.applicationName))
-    .filter((val, index, self): boolean => self.indexOf(val) === index);
-};
-
-const mapSendPorts = (
-  state: ParserState,
-  applicationName: string,
-): SendPort[] => {
-  return state.sendPorts
-    .filter((sp): boolean => sp.applicationName === applicationName)
-    .map(
-      (sp): SendPort => ({
-        name: sp.name,
-        address: sp.address,
-      }),
-    )
-    .sort((left, right): number => left.name.localeCompare(right.name));
-};
-
-const mapReceiveLocations = (
-  state: ParserState,
-  applicationName: string,
-): ReceiveLocation[] => {
-  return state.receivePorts
-    .filter((rp): boolean => rp.applicationName === applicationName)
-    .map((rp): ReceiveLocationState[] => rp.receiveLocations)
-    .reduce((prev, curr): ReceiveLocationState[] => prev.concat(curr), [])
-    .map(
-      (rl): ReceiveLocation => ({
-        name: rl.name,
-        address: rl.address,
-      }),
-    )
-    .sort((left, right): number => left.name.localeCompare(right.name));
-};
-
-const mapApplications = (state: ParserState): Application[] => {
-  return getApplicationNames(state)
-    .map(
-      (name): Application => ({
-        name,
-        sendPorts: mapSendPorts(state, name),
-        receiveLocations: mapReceiveLocations(state, name),
-      }),
-    )
-    .sort((left, right): number => left.name.localeCompare(right.name));
-};
-
-const mapParserState = (state: ParserState): Bindings => {
-  return {
-    applications: mapApplications(state),
-  };
 };
 
 const parseBindings = (data: Buffer): Promise<Bindings> => {
